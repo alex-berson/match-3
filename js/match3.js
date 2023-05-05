af7d8d1788d24a15a113c300e8611defdc53566b
Alexander Berson <aberson@yahoo.com>
2023-05-05 07:01:21 +0200

    find matches

--- js/match3.js
+++ js/match3.js
@@ -1,5 +1,5 @@
 let board, score;
-const size = 8;
+
 const fruits = {
     1:  'apple',
     2:  'tangerine',
@@ -63,8 +63,107 @@ const fillBoard = () => {
     }
 }
 
+const findMatches = (board) => {
+
+    let matches = [];
+
+    for (let r = 0; r < nRows; r++) {
+        for (let c = 0; c < nCols; c++) {
+
+            let match = [[r, c]];
+            let c2;
+
+            for (c2 = c + 1; c2 < nCols; c2++) {
+                if (board[r][c2] != board[r][c]) break;
+                match.push([r, c2]);
+            }
+
+            if (match.length >= 3) {
+                matches.push(match);
+                c = c2 - 1;
+            }
+        }
+    }
+
+    for (let c = 0; c < nCols; c++) {
+        for (let r = 0; r < nRows; r++) {
+
+            let match = [[r, c]];
+            let r2;
+
+            for (r2 = r + 1; r2 < nRows; r2++) {
+                if (board[r2][c] != board[r][c]) break;
+                match.push([r2, c]);
+            }
+
+            if (match.length >= 3) {
+                matches.push(match);
+                r = r2 - 1;
+            }
+        }
+    }
+
+    return matches;
+}
+
 const validMove = (r1,c1,r2,c2) => { 
-    return neighbour(r1,c1,r2,c2);
+
+    if (!neighbour(r1,c1,r2,c2)) return false;
+    if (board[r1][c1] == board[r2][c2]) return false;
+
+    let tempBoard = board.map(arr => arr.slice());
+
+    [tempBoard[r1][c1], tempBoard[r2][c2]] = [tempBoard[r2][c2], tempBoard[r1][c1]];
+
+    let matches = findMatches(tempBoard);
+
+    console.log(matches);
+
+
+    return matches;
+    // let [r, c] = [r1, c1];
+    // let matches = [[r, c]];
+
+    // while (r > 0) {
+
+    //     r--;
+    //     if (tempBoard[r][c] != tempBoard[r1][c1]) break;
+    //     matches.push([r,c]);
+    // }
+
+    // [r, c] = [r1, c1];
+
+    // while (r < 7) {
+
+    //     r++;
+    //     if (tempBoard[r][c] != tempBoard[r1][c1]) break;
+    //     matches.push([r,c]);
+    // }
+
+    // [r, c] = [r1, c1];
+
+    // if (matches.length < 3) matches = [[r, c]];
+
+    // while (c > 0) {
+
+    //     c--;
+    //     if (tempBoard[r][c] != tempBoard[r1][c1]) break;
+    //     matches.push([r,c]);
+    // }
+
+    // [r, c] = [r1, c1];
+
+    // while (c < 7) {
+
+    //     c++;
+    //     if (tempBoard[r][c] != tempBoard[r1][c1]) break;
+    //     matches.push([r,c]);
+    // }
+
+    // return matches.length < 3 ? null : matches;
+
+    // return true;
+
 }
 
 const neighbour = (r1,c1,r2,c2) => {
@@ -72,6 +171,32 @@ const neighbour = (r1,c1,r2,c2) => {
     if (Math.abs(r2 - r1) + Math.abs(c2 - c1) == 1) return true;
 
     return false;
+}
+
+const removeMatches = (matches) => {
+
+    console.log(matches);
+
+    let images = document.querySelectorAll('img');
+
+    for (let match of matches) {
+
+        console.log(match);
+
+        for (let item of match) {
+
+            console.log(item);
+
+            let r = item[0];
+            let c = item[1];
+
+            board[r][c] = 0;
+
+            // images[r * nCols + c].src = '';
+            images[r * nCols + c].classList.add('invisible');
+
+        }
+    }
 }
 
 const selectCell = (e) => {
@@ -97,7 +222,9 @@ const selectCell = (e) => {
 
     let [r2, c2] = [Math.trunc(i / nCols), i % nCols];
 
-    if (validMove(r,c,r2,c2)) {
+    let matches = validMove(r,c,r2,c2);
+
+    if (matches.length > 0) {
 
         [board[r][c], board[r2][c2]] = [board[r2][c2], board[r][c]];
 
@@ -106,6 +233,8 @@ const selectCell = (e) => {
         images[r2 * nCols + c2].src = tempImage;    
 
         images[i].parentElement.classList.remove('selected');
+
+        setTimeout(removeMatches, 100, matches);
 
         return;
     }
